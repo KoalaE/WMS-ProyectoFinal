976b7401daeaecc56e4cdc28c3f07c9ae1f3182d
KoalaE <duqueestefania79@gmail.com>
2026-05-25 20:39:24 -0600

    feat: movimientos, reportes y entradas/salidas funcionando

---
+++ backend/Migrations/20260526022354_AddMovimientos.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WMS.API_ProyectoFinal.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMovimientos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
